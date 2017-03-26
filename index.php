297d25187368a0b30ec8c91b6cf088f092fb235f
jazuly <ajajazuly@gmail.com>
2017-03-26 10:44:35 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<title>Perkalian Matriks</title>
+</head>
+<body>
+    <h2>Perkalian Matriks Ordo 3x3</h2>
+    <P>By : Vera Suryaningsih M0509074</p>
+    <P>Kunjungi spirit221.blogspot.com</p>
+    <form method="POST" action="rumus.php">
+        Inputkan nilai matriks pertama:<BR>
+        <INPUT TYPE = "text" NAME = "A11">
+        <INPUT TYPE = "text" NAME = "A12">
+        <INPUT TYPE = "text" NAME = "A13"><BR>
+        <INPUT TYPE = "text" NAME = "A21">
+        <INPUT TYPE = "text" NAME = "A22">
+        <INPUT TYPE = "text" NAME = "A23"><BR>
+        <INPUT TYPE = "text" NAME = "A31">
+        <INPUT TYPE = "text" NAME = "A32">
+        <INPUT TYPE = "text" NAME = "A33"><BR>
+        Inputkan nilai matriks kedua:<BR>
+        <INPUT TYPE = "text" NAME = "B11">
+        <INPUT TYPE = "text" NAME = "B12">
+        <INPUT TYPE = "text" NAME = "B13"><BR>
+        <INPUT TYPE = "text" NAME = "B21">
+        <INPUT TYPE = "text" NAME = "B22">
+        <INPUT TYPE = "text" NAME = "B23"><BR>
+        <INPUT TYPE = "text" NAME = "B31">
+        <INPUT TYPE = "text" NAME = "B32">
+        <INPUT TYPE = "text" NAME = "B33"><BR>
+        <INPUT TYPE="submit" VALUE="hitung" NAME="submit">
+    </form>
+</body>
+</html>
